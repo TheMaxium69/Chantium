f4f65dfbc993f0a35591ff9726c58d472abaaff9
norman <rayquacondaytb@gmail.com>
2021-10-14 14:45:07 +0200

    Orthographe

--- migrations/Version20211014123155.php
+++ migrations/Version20211014123155.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211011121858 extends AbstractMigration
+final class Version20211014123155 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,15 +21,13 @@ final class Version20211011121858 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, adresse VARCHAR(255) NOT NULL, dateend DATETIME NOT NULL, datestart DATETIME NOT NULL, client VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64986CC499D ON user (pseudo)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, pseudo VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, phonenumber VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, is_account TINYINT(1) NOT NULL, content VARCHAR(255) DEFAULT NULL, role JSON DEFAULT NULL, created_at DATETIME NOT NULL, username VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE project');
-        $this->addSql('DROP INDEX UNIQ_8D93D64986CC499D ON user');
-        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+        $this->addSql('DROP TABLE user');
     }
 }
